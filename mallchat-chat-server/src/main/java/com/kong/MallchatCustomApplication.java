ed3177239b59611cb7205d9a79231cb5d9e105ed
孔德玲 <66666666@qq.com>
2024-07-18 11:19:29 +0800

    ceshi

--- mallchat-chat-server/src/main/java/com/kong/MallchatCustomApplication.java
+++ mallchat-chat-server/src/main/java/com/kong/MallchatCustomApplication.java
@@ -8,4 +8,8 @@ public class MallchatCustomApplication {
         SpringApplication.run(MallchatCustomApplication.class,args);
     }
 
+    public static void test(){
+        String ceshi ="ceshidaimaheur"
+    }
+
 }
