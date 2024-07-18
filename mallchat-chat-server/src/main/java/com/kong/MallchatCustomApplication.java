4599619a586a5a4ebf8c8a77a7467c3dad6d6f0a
孔德玲 <66666666@qq.com>
2024-07-18 11:11:24 +0800

    初始化

---
+++ mallchat-chat-server/src/main/java/com/kong/MallchatCustomApplication.java
@@ -0,0 +1,11 @@
+package com.kong;
+
+import org.springframework.boot.SpringApplication;
+
+public class MallchatCustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallchatCustomApplication.class,args);
+    }
+
+}
